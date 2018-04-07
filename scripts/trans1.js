173ce3b1b1cb41761dabdc7ea0acda365a1d4de6
kimpossible6 <thestars.thesnowandthefire@gmail.com>
2018-04-07 14:22:53 -0400

    update3
    * new paitient: name, address, dob
    * previous patient: phone number
    * some adjustments of the color of the button: change all proceed to green and exit to red
    * todo: javascript: confirm info for each input page

--- scripts/trans1.js
+++ scripts/trans1.js
@@ -1,6 +1,44 @@
-$(document).ready(function() {
-    $('camera').fadeOut('slow');
+
+
+// auto input flow to next
+$(".inputs").keyup(function () {
+   if (this.value.length == this.maxLength) {
+     $(this).nextAll('.inputs:enabled:first').focus();
+   }
 });
-// function switchvisible() {
-// 	if (document.getElementById(''))
+
+$(function(){
+    var $select = $(".1-12");
+    var $select2 = $(".1-31");
+    var $select3 = $(".1900-2018");
+    for (i=1;i<=12;i++){
+        $select.append($('<option></option>').val(i).html(i))
+    }
+    for (i=1;i<=31;i++){
+        $select2.append($('<option></option>').val(i).html(i))
+    }
+    for (i=1900;i<=2018;i++){
+        $select3.append($('<option></option>').val(i).html(i))
+    }
+});
+
+
+var input1=123;
+var input2=123;
+var input3=1234;
+function myfunction() {
+	input1 = document.getElementById("tel1").innerHTML;
+    input2 = document.getElementById("tel2").innerHTML;
+    input3 = document.getElementById("tel3").innerHTML;
+    window.location.href = 'confirm_phone.html';
+    return [input1,input2,input3];
+}
+
+// function getphone() {
+// 	var code = new myfunction();
+// 	return 
 // }
+
+function getdate() {
+    var dob = document.getElementById("myDate").value;
+}
